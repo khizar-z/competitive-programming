fc2f4d1cb8d8228aab25b4442f2e413ee9561836
khizar-z <khiz.zaman@gmail.com>
2023-03-08 18:41:08 -0500

    CSES - Repetitions

--- CSES/introductory-problems/repetitions.cpp
+++ CSES/introductory-problems/repetitions.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+int main() {
+    string sequence;
+    cin >> sequence;
+    int n = sequence.length(), best = 0, curr = 1;
+    for (int i = 1; i < n; i++) {
+        if (sequence[i] != sequence[i - 1])
+        {
+            best = max(best, curr);
+            curr = 0;
+        }
+        curr++;
+    }
+    best = max(best, curr);
+    cout << best;
+    return 0;
+}
+/*
+#include <iostream>/
+#include <string>
 #include <vector>
 #include <algorithm>
 
@@ -8,8 +29,11 @@ using namespace std;
 int main() {
   string sequence;
   cin >> sequence;
-  int aLongest, cLongest, gLongest, tLongest;
-  for (int i = 0; i < sequence.size(); i++) {
+  int aLongest = 0;
+  int cLongest = 0;
+  int gLongest = 0;
+  int tLongest = 0;
+  for (int i = 0; i <= sequence.size(); i++) {
     if (sequence[i] == sequence[i + 1]) {
       switch(sequence[i]) {
         case 'A':
@@ -29,5 +53,6 @@ int main() {
   finalNumbers.push_back(gLongest);
   finalNumbers.push_back(tLongest);
   sort(finalNumbers.begin(), finalNumbers.end());
-  cout << finalNumbers[0];
+  cout << finalNumbers[3];
 }
+*/
