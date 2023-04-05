a6753bf2302728be0c183133cfa832c357c45adc
khizar <89149771+khizar-z@users.noreply.github.com>
2023-04-05 13:35:43 +0000

    CCC - S2 - Tandem Bicycle

--- CCC/S2/tandem-bicycle.cpp
+++ CCC/S2/tandem-bicycle.cpp
@@ -5,5 +5,27 @@
 using namespace std;
 
 int main() {
-  cout << "Hi";
+  int probType, n;
+  cin >> probType >> n;
+  vector<int> dmojistan(n), pegland(n);
+  for (int i = 0; i < n; i++) {
+    cin >> dmojistan[i];
+  }
+  for (int i = 0; i < n; i++) {
+    cin >> pegland[i];
+  }
+  
+  vector<int> final(n);
+  sort(dmojistan.begin(), dmojistan.end());
+  sort(pegland.begin(), pegland.end());
+  if (probType == 1) {
+    for (int i = 0; i < n; i++) {
+      final[i] = max(dmojistan[i], pegland[i]);
+    }
+  } else if (probType == 2) {
+    for (int i = 0; i < n; i++) {
+      final[i] = max(dmojistan[i], pegland[n - (i + 1)]);
+    }
+  }
+  cout << accumulate(final.begin(), final.end(), 0) << "\n";
 }
