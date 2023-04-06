db237c53a32cb097f24f2dd004f17b87cfdd8582
khizar <89149771+khizar-z@users.noreply.github.com>
2023-04-06 22:51:36 +0000

    Started Out Of Place (Codespaces)

---
+++ USACO/Bronze/Problem 3/out-of-place.cpp
@@ -0,0 +1,16 @@
+// USACO 2018 January Contest, Bronze, Problem 3
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  set<int> values;
+  for (int i = 0; i < n; i++) {
+    int m;
+    cin >> m;
+    values.insert(m);
+  }
+}
